0b8f81233ba4177ee348406ef119914fa29240de
Hoangtuantu1234 <hipham16@gmail.com>
2025-11-14 21:15:58 +0700

    Update sidebar.php

--- views/admin/sidebar.php
+++ views/admin/sidebar.php
@@ -1,42 +1,27 @@
-<!doctype html>
-<html lang="en">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+<div class="d-flex">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Sidebar Layout</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+    <!-- Sidebar -->
+    <div style="width: 240px; background: #f8f9fa; min-height: 100vh;" class="p-3 border-end">
 
-<body>
+        <!-- Logo -->
+        <img src="https://insacmau.com/wp-content/uploads/2023/02/logo-fpt-polytechnic.png" alt="Logo FPT" width="150"
+            class="mb-4">
 
-    <div class="d-flex">
+        <div class="list-group" style="margin-top: 20px;">
+            <a href="#" class="list-group-item list-group-item-action">Quản lý tour và sản phẩm du lịch</a>
+            <a href="#" class="list-group-item list-group-item-action">Bán tour và đặt chỗ</a>
+            <a href="#" class="list-group-item list-group-item-action">Quản lý & điều hành tour </a>
+            <a href="#" class="list-group-item list-group-item-action">Quản lý đối tác, nhà cung cấp</a>
+            <a href="#" class="list-group-item list-group-item-action">Quản lý tài chính liên quan tour</a>
+            <a href="#" class="list-group-item list-group-item-action">Báo cáo vận hành tour</a>
+        </div>
+        <div>
 
-        <!-- Sidebar -->
-        <div style="width: 240px; background: #f8f9fa; min-height: 100vh;" class="p-3 border-end">
-
-            <!-- Logo -->
-            <img src="https://insacmau.com/wp-content/uploads/2023/02/logo-fpt-polytechnic.png" alt="Logo FPT"
-                width="150" class="mb-4">
-
-            <div class="list-group" style="margin-top: 20px;">
-                <a href="#" class="list-group-item list-group-item-action">Quản lý tour và sản phẩm du lịch</a>
-                <a href="#" class="list-group-item list-group-item-action">Bán tour và đặt chỗ</a>
-                <a href="#" class="list-group-item list-group-item-action">Quản lý & điều hành tour </a>
-                <a href="#" class="list-group-item list-group-item-action">Quản lý đối tác, nhà cung cấp</a>
-                <a href="#" class="list-group-item list-group-item-action">Quản lý tài chính liên quan tour</a>
-                <a href="#" class="list-group-item list-group-item-action">Báo cáo vận hành tour</a>
-            </div>
-            <div>
-
-                <button class="btn btn-primary mt-auto">Đăng xuất</button>
-            </div>
-
-
+            <a href="<?= BASE_URL ?>"><button class="btn btn-primary mt-auto">Đăng xuất</button></a>
         </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 
-</body>
+    </div>
 
-</html>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
