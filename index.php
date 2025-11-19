990503672491ebd1ea97e3462698ff17e8e8b35f
hoaithu <phamhoaithu02112006@gmail.com>
2025-11-19 08:55:26 +0700

    commit index new

--- index.php
+++ index.php
@@ -7,9 +7,11 @@ require_once './commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
 require_once './controllers/ProductController.php';
+require_once 'controllers/HDVController.php';
 
 // Require toàn bộ file Models
 require_once './models/ProductModel.php';
+require_once './models/HDVModel.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
