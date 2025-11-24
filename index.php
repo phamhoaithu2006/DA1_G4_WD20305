1c43ad1e9afe3f174ca33dd97db5ed5ff46e5c6f
thaind <thaiinguyenn20112006@gmail.com>
2025-11-24 08:09:16 +0700

    sua index

--- index.php
+++ index.php
@@ -7,16 +7,25 @@ require_once './commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
 require_once './controllers/ProductController.php';
+require_once './controllers/BookingController.php';
 require_once 'controllers/HDVController.php';
+require_once 'controllers/EmployeeController.php';
+require_once 'controllers/TourAssignmentController.php';
+require_once 'controllers/TourCustomerController.php';
 
 // Require toàn bộ file Models
 require_once './models/ProductModel.php';
+require_once './models/Booking.php';
 require_once './models/HDVModel.php';
+require_once './models/EmployeeModel.php';
+require_once './models/TourAssignmentModel.php';
+require_once './models/TourCustomerModel.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
 $id = $_GET['id'] ?? '';
-
+$db = connectDB();
+$tourID = isset($_GET['tourID']) ? intval($_GET['tourID']) : null;
 
 // Để bảo bảo tính chất chỉ gọi 1 hàm controller để xử lý request thì mình sử dụng match
 
@@ -28,6 +37,22 @@ match ($act) {
     'category' => (new ProductController())->adminDashboard(),
     'detail' => (new ProductController())->adminDetail($id),
 
+    // Trang booking
+    'booking-list' => (new BookingController($db))->index(),
+    'booking-detail' => (new BookingController($db))->detail($id),
+    'booking-create' => (new BookingController($db))->create(),
+
+    // Điều hành
+    'employees' => (new EmployeeController())->index(),
+    'createEmployee' => (new EmployeeController())->create(),
+    'editEmployee' => (new EmployeeController())->edit($id),
+    'deleteEmployee' => (new EmployeeController())->delete($id),
+    'assignments' => (new TourAssignmentController())->index($tourID),
+    'createAssignment' => (new TourAssignmentController())->create($tourID),
+    'deleteAssignment' => (new TourAssignmentController())->delete($_GET['id'], $tourID),
+    'tourcustomers' => (new TourCustomerController())->index($tourID),
+    'createTourCustomer' => (new TourCustomerController())->create($tourID),
+
     // Trang hdv
     'hdv-login' => (new HDVController())->login(),
     'hdv-check-login' => (new HDVController())->checkLogin(),
@@ -38,6 +63,7 @@ match ($act) {
     // Nhật ký & cập nhật tour của hdv
     'hdv-diary-form' => (new HDVController())->diaryForm(),
     'hdv-diary-save' => (new HDVController())->diarySave(),
+    'hdv-diary-delete' => (new HDVController())->diaryDelete(),
     'hdv-checkin-checkout' => (new HDVController())->checkInOut(),
     'hdv-checkin-save' => (new HDVController())->checkInSave(),
     'hdv-checkout-save' => (new HDVController())->checkOutSave(),
