8d2d6eb2c8698a993318eef589800333e5fb9b82
Hoangtuantu1234 <hipham16@gmail.com>
2025-11-24 12:20:35 +0700

    Update env.php

--- commons/env.php
+++ commons/env.php
@@ -3,12 +3,12 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng hằng số để không phải dùng $GLOBALS
 
-define('BASE_URL', 'http://localhost/Team_word_1/DA1_G4_WD20305/');
+define('BASE_URL', 'http://localhost/DA1_G4_WD20305/DA1_G4_WD20305-main/');
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'tourmanagement');  // tên database 
+define('DB_NAME', 'trang_du_lich');  // tên database 
 
 define('PATH_ROOT', __DIR__ . '/../');
