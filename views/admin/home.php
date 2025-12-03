ffb3d629d13dcd4636b957347985ede0c6a59db9
hoaithu <phamhoaithu02112006@gmail.com>
2025-12-03 08:11:38 +0700

    new commit

--- views/admin/home.php
+++ views/admin/home.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . '/../main.php'; ?>
+<?php require_once __DIR__ . '/main.php'; ?>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
